a8fc3aaa3484946c3491facd177374cb1230da17
Y.K. Goon <ykgoon@gmail.com>
2024-10-30 23:32:11 +0800

    Built successfully
    build: update Compose BOM to 2024.10.01 to resolve deprecation warnings
    
    Almost built successfully.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
 
     defaultConfig {
         applicationId = "com.ykgoon.torrid"
-        minSdk = 26
+        minSdk = 28
         targetSdk = 34
         versionCode = 1
         versionName = "0.1"
@@ -42,25 +42,35 @@ android {
     }
     buildFeatures {
         compose = true
+        buildConfig = true
     }
 }
 
 dependencies {
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
-    implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.media3:media3-exoplayer:1.4.1")
+    implementation("androidx.media3:media3-ui:1.4.1")
+    implementation("androidx.navigation:navigation-compose:2.8.2")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.8.2")
+    implementation("androidx.navigation:navigation-ui-ktx:2.8.2")
+
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.6")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.6")
+    implementation("androidx.activity:activity-compose:1.9.2")
+    implementation(platform("androidx.compose:compose-bom:2024.10.01"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material:material")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.1")
+    implementation("androidx.compose.material:material-icons-core:1.7.4")
 
     // Meta Spatial SDK libs
     implementation("com.meta.spatial:meta-spatial-sdk:$metaSpatialSdkVersion")
@@ -71,11 +81,40 @@ dependencies {
     implementation("com.meta.spatial:meta-spatial-sdk-mruk:$metaSpatialSdkVersion")
     implementation("com.meta.spatial:meta-spatial-sdk-castinputforward:$metaSpatialSdkVersion")
 
-    androidTestImplementation(platform("androidx.compose:compose-bom:2024.09.03"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.10.01"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
     ksp("com.google.code.gson:gson:2.11.0")
     ksp("com.meta.spatial.plugin:com.meta.spatial.plugin.gradle.plugin:$metaSpatialSdkVersion")
+}
+
+afterEvaluate {
+    tasks.named("assembleDebug") {
+        dependsOn("export")
+        finalizedBy("generateComponents")
+    }
+}
+
+val sceneProjectPath = "app/scenes"
+
+spatial {
+    allowUsageDataCollection.set(true)
+    scenes {
+        // if you have installed Meta Spatial Editor somewhere else, update the file path.
+        // cliPath.set("/Applications/Meta Spatial Editor.app/Contents/MacOS/CLI")
+        exportItems {
+            item {
+                projectPath.set("$sceneProjectPath/Main.metaspatial")
+                outputPath.set("app/src/main/assets/scenes")
+            }
+        }
+        componentGeneration {
+            outputPath.set(sceneProjectPath)
+            // We attempt to auto-detect where your "custom_components.json" is placed but if this does
+            // not work then you can uncomment the following line and force it to a specific location.
+            // customComponentsPath.set("app/build/generated/ksp/debug/resources/custom_components.json")
+        }
+    }
 }
--- app/src/main/java/com/ykgoon/torrid/MainActivity.kt
+++ app/src/main/java/com/ykgoon/torrid/MainActivity.kt
@@ -1,47 +1,102 @@
 package com.ykgoon.torrid
 
+import android.net.Uri
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.ykgoon.torrid.ui.theme.TorridTheme
+import com.meta.spatial.castinputforward.CastInputForwardFeature
+import com.meta.spatial.core.Entity
+import com.meta.spatial.core.Pose
+import com.meta.spatial.core.SpatialFeature
+import com.meta.spatial.core.Vector3
+import com.meta.spatial.okhttp3.OkHttpAssetFetcher
+import com.meta.spatial.runtime.NetworkedAssetLoader
+import com.meta.spatial.runtime.ReferenceSpace
+import com.meta.spatial.runtime.SceneMaterial
+import com.meta.spatial.toolkit.AppSystemActivity
+import com.meta.spatial.toolkit.Material
+import com.meta.spatial.toolkit.Mesh
+import com.meta.spatial.toolkit.PanelRegistration
+import com.meta.spatial.toolkit.Transform
+import com.meta.spatial.vr.VRFeature
+import java.io.File
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppSystemActivity() {
+    private var gltfxEntity: Entity? = null
+    private val activityScope = CoroutineScope(Dispatchers.Main)
+
+    override fun registerFeatures(): List<SpatialFeature> {
+        val features = mutableListOf<SpatialFeature>(VRFeature(this))
+        if (BuildConfig.DEBUG) {
+            features.add(CastInputForwardFeature(this))
+        }
+        return features
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            TorridTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+        NetworkedAssetLoader.init(
+            File(applicationContext.getCacheDir().canonicalPath), OkHttpAssetFetcher())
+
+        // wait for GLXF to load before accessing nodes inside it
+        loadGLXF().invokeOnCompletion {
+            // get the environment mesh from Cosmo and set it to use an unlit shader.
+            val composition = glXFManager.getGLXFInfo("entity_prime")
+            val environmentEntity: Entity? = composition.getNodeByName("Environment").entity
+            val environmentMesh = environmentEntity?.getComponent<Mesh>()
+            environmentMesh?.defaultShaderOverride = SceneMaterial.UNLIT_SHADER
+            environmentEntity?.setComponent(environmentMesh!!)
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    override fun onSceneReady() {
+        super.onSceneReady()
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TorridTheme {
-        Greeting("Android")
+        // set the reference space to enable recentering
+        scene.setReferenceSpace(ReferenceSpace.LOCAL_FLOOR)
+
+        scene.setLightingEnvironment(
+            ambientColor = Vector3(0f),
+            sunColor = Vector3(7.0f, 7.0f, 7.0f),
+            sunDirection = -Vector3(1.0f, 3.0f, -2.0f),
+            environmentIntensity = 0.3f)
+        scene.updateIBLEnvironment("environment.env")
+
+        scene.setViewOrigin(0.0f, 0.0f, 2.0f, 180.0f)
+
+        Entity.create(
+            listOf(
+                Mesh(Uri.parse("mesh://skybox")),
+                Material().apply {
+                    baseTextureAndroidResourceId = R.drawable.skydome
+                    unlit = true // Prevent scene lighting from affecting the skybox
+                },
+                Transform(Pose(Vector3(x = 0f, y = 0f, z = 0f)))))
+    }
+
+    private fun loadGLXF(): Job {
+        gltfxEntity = Entity.create()
+        return activityScope.launch {
+            glXFManager.inflateGLXF(
+                Uri.parse("apk:///scenes/Composition.glxf"),
+                rootEntity = gltfxEntity!!,
+                keyName = "entity_prime")
+        }
+    }
+
+    override fun registerPanels(): List<PanelRegistration> {
+        return listOf(
+            PanelRegistration(R.layout.prime) {
+                config {
+                    themeResourceId = R.style.PanelAppThemeTransparent
+                    includeGlass = false
+                    width = 2.0f
+                    height = 1.5f
+                    enableLayer = true
+                    enableTransparent = true
+                }
+            })
     }
 }
